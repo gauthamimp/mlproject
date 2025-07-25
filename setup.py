0b41915fd14411fb976c4f8d64be9f7cd01153c1
gauthamimp <gauthamimuchal601@gmail.com>
2025-07-25 15:25:19 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+## this setup.py will be responsible in creating my machine learning application as a package.
+from setuptools import find_packages,setup
+from typing import List
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='gauthamimp',
+    author_email='gauthamimuchal601@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
