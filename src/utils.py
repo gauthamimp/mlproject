7576d2759dab86faaaa4150df02aab64716d9947
gauthamimp <gauthamimuchal601@gmail.com>
2025-07-26 12:59:58 +0530

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -21,12 +21,18 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-def evaluate_models(x_train, y_train,x_test,y_test,models):
+def evaluate_models(x_train, y_train,x_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
@@ -45,5 +51,3 @@ def evaluate_models(x_train, y_train,x_test,y_test,models):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
-    
